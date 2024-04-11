30923a9f4a408fc3825d83b90d3a939c4b846655
1matux1 <mateusz.kmak@onet.pl>
2024-04-11 09:46:38 +0200

    Dodaj pliki projektów.

---
+++ SortowanieGIT/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortowanieGIT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 5, 3, 1, 4, 6 };
+            Console.WriteLine("Przed sortowaniem: " + string.Join(", ", array));
+
+            Console.ReadKey(true);
+        }
+    }
+}

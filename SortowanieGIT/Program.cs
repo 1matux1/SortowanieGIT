0de05dcbf92c17378711f7f367d598a2d566d979
1matux1 <mateusz.kmak@onet.pl>
2024-04-11 09:58:08 +0200

    Dodanie pobrania tablicy od użytkownika

--- SortowanieGIT/Program.cs
+++ SortowanieGIT/Program.cs
@@ -10,13 +10,27 @@ namespace SortowanieGIT
     {
         static void Main(string[] args)
         {
-            int[] array = { 5, 3, 1, 4, 6 };
+            int[] array = GetArrayFromUser();
             Console.WriteLine("Przed sortowaniem: " + string.Join(", ", array));
-
             BubbleSort(array);
             Console.WriteLine("Po sortowaniu: " + string.Join(", ", array));
 
             Console.ReadKey(true);
+        }
+
+        static int[] GetArrayFromUser()
+        {
+            Console.WriteLine("Podaj liczbę elementów tablicy:");
+            int size = int.Parse(Console.ReadLine());
+
+            int[] array = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine($"Podaj element {i + 1}:");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            return array;
         }
 
         static void BubbleSort(int[] array)

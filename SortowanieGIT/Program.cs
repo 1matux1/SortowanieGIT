8dc726b61a5ca492633d3f67a0d70d23d2392480
1matux1 <mateusz.kmak@onet.pl>
2024-04-11 09:53:04 +0200

    Dodanie metody BubbleSort

--- SortowanieGIT/Program.cs
+++ SortowanieGIT/Program.cs
@@ -13,7 +13,27 @@ namespace SortowanieGIT
             int[] array = { 5, 3, 1, 4, 6 };
             Console.WriteLine("Przed sortowaniem: " + string.Join(", ", array));
 
+            BubbleSort(array);
+            Console.WriteLine("Po sortowaniu: " + string.Join(", ", array));
+
             Console.ReadKey(true);
+        }
+
+        static void BubbleSort(int[] array)
+        {
+            int temp;
+            for (int j = 0; j <= array.Length - 2; j++)
+            {
+                for (int i = 0; i <= array.Length - 2; i++)
+                {
+                    if (array[i] > array[i + 1])
+                    {
+                        temp = array[i + 1];
+                        array[i + 1] = array[i];
+                        array[i] = temp;
+                    }
+                }
+            }
         }
     }
 }
